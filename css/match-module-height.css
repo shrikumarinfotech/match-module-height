96fe6e0b0976836efa716fe987a318f49cfa7862
Debraj Rakshit <debrajweb@gmail.com>
2020-10-28 17:51:22 +0530

    credits updated

--- css/match-module-height.css
+++ css/match-module-height.css
@@ -6,7 +6,7 @@
 // Author Email: dev@shrikumarinfotech.com
 // Date: 28 October 2020
 // Update Date: 28 October 2020
-// Credits: https://github.com/debrajrakshit/jquery-sort-ascending-descending
+// Contributors: https://github.com/debrajrakshit/jquery-sort-ascending-descending
 // License: https://opensource.org/licenses/MIT
 // Copyright 2020, https://github.com/shrikumarinfotech/match-module-height
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
